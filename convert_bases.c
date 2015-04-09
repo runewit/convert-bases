f03da9a4d17cdaf160fdce453858d74bdc223da0
Jared Curry <jayben7@gmail.com>
2015-04-09 16:28:57 -0700

    finished project

--- convert_bases.c
+++ convert_bases.c
@@ -19,6 +19,18 @@ int character_to_decimal(char character){ //,int base_from,int i){
 	return d_number;
 }
 
+char decimal_to_character(int decimal){
+	char c_number, output;
+	if(decimal<9 && decimal>=0) {
+		c_number=48+decimal;
+		output=(char)c_number;
+	} else if(decimal>9) {
+		c_number=87+decimal;
+		output=(char)c_number;
+	}
+	return output;
+}
+
 int convert_to_dec(char* number,int base_from){
 	int i, k;
 	char decimal_array[65];
@@ -26,31 +38,35 @@ int convert_to_dec(char* number,int base_from){
 	int length;
 	for(i=0; number[i]!='\0'; i++){
 		decimal_array[i]=character_to_decimal(number[i]); //,base_from,i*base_from^i);
-		printf("%c -> %d\n",number[i],decimal_array[i]);
+		//printf("%c -> %d\n",number[i],decimal_array[i]);
 	}
-	length=(strlen(decimal_array));
-	printf("%d \n",length);
+	length=i;
+	//printf("%d \n",length);
 	for(k=0; decimal_array[k]!='\0'; k++){
 		decimal_number+=decimal_array[k]*power(base_from,(length-(k+1)));
 	}
-	printf("The number in base 10 is: %d \n",decimal_number);
+	//printf("The number in base 10 is: %d \n",decimal_number);
 	return decimal_number;
 }
 
 void convert_to_new(int number, int base_to){
-	int remainders[65], n, m, p, dividingstuff;
+	int remainders[65], n, m, p, d, dividingstuff;
 	char final_num[65];
+	for(d=0;d<=64;d++){
+		remainders[d]=-1;
+	}
 	dividingstuff=number;
 	for(n=64; dividingstuff!=0;n--){
-		printf("dividingstuff before: %i ",dividingstuff);
+		//printf("dividingstuff before: %i ",dividingstuff);
 		remainders[n]=dividingstuff%base_to;
 		dividingstuff /= base_to;
-		printf("dividingstuff after: %i, remainder: %i \n",dividingstuff,remainders[n]);
+		//printf("dividingstuff after: %i, remainder: %i \n",dividingstuff,remainders[n]);
 	};
-	//for(p=64;remainders[p]!=0;p--){
-	while(remainders[p]=='\0'){
-		printf("%i, ", remainders[p]); // something's not right here
-		p++;
+	for(p=0;p<=64;p++){
+		if(remainders[p]==-1){
+		}else{
+			printf("%c",decimal_to_character(remainders[p]));
+		};
 	};
 }
 
@@ -67,10 +83,11 @@ int main(int argc, char**argv){
 	printf("what base shall I convert it to? ");
 	scanf("%i" , &base_to);
 	
-	printf("%s, %i, %i \n",number,base_from,base_to);
+	//printf("%s, %i, %i \n",number,base_from,base_to);
 	
 	decimal_form=convert_to_dec(number,base_from);
+	printf("Converted form is: ");
 	convert_to_new(decimal_form,base_to);
-	printf("the converted number is the backwards order of the remainders \n");
+	printf("\n");
 	return 0;
 }
